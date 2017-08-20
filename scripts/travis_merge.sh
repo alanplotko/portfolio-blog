33725e8999e39c7b754e4c71444f054243f8d922
Alan Plotko <alan.plotko@gmail.com>
2017-08-19 22:48:11 -0400

    Fix deployment script

--- scripts/travis_merge.sh
+++ scripts/travis_merge.sh
@@ -19,13 +19,16 @@ git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
 # shellcheck disable=SC2164
 cd "$repo_temp"
 
-printf 'Checking out %s\n' "$BRANCH_TO_MERGE_INTO" >&2
+printf 'Checking out [%s]\n' "$BRANCH_TO_MERGE_INTO" >&2
 git checkout -b "$BRANCH_TO_MERGE_INTO"
+git remote add upstream "https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO" >/dev/null 2>&1
 
-printf 'Cleaning up for release\n' >&2
+printf 'Merging %s\n' "$TRAVIS_COMMIT" >&2
+git merge --ff-only "$TRAVIS_COMMIT"
+
+printf 'Cleaning up [%s] for release\n' "$BRANCH_TO_MERGE_INTO" >&2
 git config user.name $GIT_COMMITTER_NAME >/dev/null 2>&1
 git config user.email $GIT_COMMITTER_EMAIL >/dev/null 2>&1
-git remote add upstream "https://$GITHUB_SECRET_TOKEN@github.com/$GITHUB_REPO" >/dev/null 2>&1
 git rm -rf scripts/
 git rm -rf test/
 git rm README.md
@@ -43,5 +46,5 @@ git add index.html
 git commit -m "Clean up for build #$TRAVIS_BUILD_NUMBER to [$BRANCH_TO_MERGE_INTO]"
 
 # Redirect to /dev/null to avoid secret leakage
-printf 'Pushing to %s\n' "$GITHUB_REPO" >&2
-git push -q upstream +$BRANCH_TO_MERGE_INTO:$BRANCH_TO_MERGE_INTO >/dev/null 2>&1
+printf 'Deploying to [%s]\n' "$BRANCH_TO_MERGE_INTO" >&2
+git push -qu upstream $BRANCH_TO_MERGE_INTO --delete $BRANCH_TO_MERGE_INTO >/dev/null 2>&1
