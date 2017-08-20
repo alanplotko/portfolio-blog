2b4ab020cfa54dbba10f078a97f83e9402fdc65d
Alan <alan.plotko@gmail.com>
2017-08-20 16:41:39 +0000

    Clean up for build #46 to [stage]

--- assets/js/setup.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  particlesJS.load('particles-js');
-  $('.email-btn').click(function() {
-    $('p.note').fadeToggle('fast', 'linear');
-  });
-});
--- test/csslint.spec.js
+++
@@ -1,4 +0,0 @@
-/**
- * CSS Linting
- */
-require('mocha-csslint')(['assets/css/style.css']);
